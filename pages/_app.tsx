4658c4384ee35fe33dd97af10f2e26a68aad4a39
mahram.gurbanzade <mgurbanzade@yahoo.com>
2022-10-29 13:29:17 +0400

    Add login component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react' 
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Layout from '@components/Layout';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>    
-  )
+    <Layout>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </Layout>
+  );
 }
