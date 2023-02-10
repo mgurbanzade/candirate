c9887c56a3ca5fadc8cb08ec861f9d69609f0e55
mahram.gurbanzade <mgurbanzade@yahoo.com>
2023-02-10 20:50:09 +0400

    Fix plan

--- components/LandingPage/Pricing.tsx
+++ components/LandingPage/Pricing.tsx
@@ -68,39 +68,33 @@ function Plan({
   return (
     <section
       className={clsx(
-        'flex flex-col rounded-3xl px-6 sm:px-8',
-        featured ? 'order-first bg-blue-600 py-8 lg:order-none' : 'lg:py-8',
+        'flex flex-col rounded-3xl px-6 sm:px-8 mx-4',
+        featured
+          ? 'order-first bg-blue-600 py-8 lg:order-none'
+          : 'lg:py-8 bg-indigo-600 ',
       )}
     >
       <h3 className="mt-5 font-display text-lg text-white">{name}</h3>
-      <p
-        className={clsx(
-          'mt-2 text-base',
-          featured ? 'text-white' : 'text-slate-400',
-        )}
-      >
-        {description}
-      </p>
+      <p className={clsx('mt-2 text-base text-white')}>{description}</p>
       <p className="order-first font-display text-5xl font-light tracking-tight text-white">
         {price}
       </p>
       <ul
         role="list"
         className={clsx(
-          'order-last mt-10 flex flex-col gap-y-3 text-sm',
-          featured ? 'text-white' : 'text-slate-200',
+          'order-last mt-10 flex flex-col gap-y-3 text-sm text-white',
         )}
       >
         {features.map((feature: string) => (
           <li key={feature} className="flex">
-            <CheckIcon className={featured ? 'text-white' : 'text-slate-400'} />
+            <CheckIcon className="text-white" />
             <span className="ml-4">{feature}</span>
           </li>
         ))}
       </ul>
       <Button
         href={href}
-        variant={featured ? 'solid' : 'outline'}
+        variant="solid"
         color="white"
         className="mt-8"
         aria-label={`Get started with the ${name} plan for ${price}`}
@@ -128,8 +122,7 @@ export function Pricing() {
             for everyone.
           </h2>
           <p className="mt-4 text-lg text-slate-400">
-            It doesn’t matter what size your business is, our software won’t
-            work well for you.
+            Pick your side and see how things can become easy.
           </p>
         </div>
         <div className="-mx-4 mt-16 flex justify-center">
